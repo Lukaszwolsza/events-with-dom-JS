62d5d582f84a7f9402faab873f558e59a6d4f9bc
Lukaszwolsza <luasply@gmail.com>
2020-06-27 17:00:26 +0200

    getting random number from function

--- functionsApp.js
+++ functionsApp.js
@@ -1,11 +1,36 @@
-const createTaxCalc = tax =>{
-    const caclulateTax = amount =>{
-        return amount * tax;
-    };
-    return caclulateTax;
+// const createTaxCalc = tax =>{
+//     const caclulateTax = amount =>{
+//         return amount * tax;
+//     };
+//     return caclulateTax;
+// };
+
+// const calculateVatAmount = createTaxCalc(0.25);
+// const calculateIncomeTaxAmount = createTaxCalc(0.19);
+
+// console.log(calculateVatAmount(200));
+
+const randomNumberDisplayed = document.querySelector('#randNum');
+const randomTheNumberBtn = document.querySelector('#randomTheNumberBtn');
+const resultOfRandom = document.querySelector('#resultOfRandom');
+
+let counter = 1;
+
+const randomNumber = (min, max) =>{
+    return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const calculateVatAmount = createTaxCalc(0.25);
-const calculateIncomeTaxAmount = createTaxCalc(0.19);
+const showRandomizeNumber = () => {
+    const h3 = document.createElement('h3');
+    h3.innerText = counter + '. Random number is : ';
 
-console.log(calculateVatAmount(200));
+    const span = document.createElement('span');
+    span.setAttribute('id','randNum');
+    span.innerText =  randomNumber(1,100);
+
+    resultOfRandom.append(h3);
+    h3.append(span);
+    counter++;
+};
+
+randomTheNumberBtn.addEventListener('click', showRandomizeNumber);
